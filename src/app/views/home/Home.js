16fc4ccfa372b52bde45b4bbb8134cf0954d4864
Cesar Carreras <cesar.carreras95@gmail.com>
2021-07-10 13:02:41 -0500

    Home update

--- src/app/views/home/Home.js
+++ src/app/views/home/Home.js
@@ -2,24 +2,40 @@ import React from 'react';
 import './styles.css'
 import logo from '../../assets/images/logo.png'
 import {Link}  from 'react-router-dom'
-import { Slide } from '../../components';
+import { Footer, Navbar, Slide, HomeIcons } from '../../components';
+
 
                 //react-router-dom
-const Home = ({history,location,match}) => (
-
+const Home = ({}) => (
+    <section>
+    <Navbar/>
     <section className="home-container" >
-    <Slide/>
-        <div className="title-section">
-            <img src={logo} width="30%" alt=""/>
-            <h1 className="title-text">
-                Best Software tool for Veterinary
-            </h1>
-            <span>
-                <Link to="/login">Login</Link> | <Link to="/signup">Signup</Link>
-            </span>
-        </div>
+    <Slide>
+    </Slide>
+    <div>
+    <h1>Sistema para veterinaria</h1>
+    <h3>Software Gestión de clinicas Veterinarias</h3>
+    </div>
+    <section className="home-icons">
+    <HomeIcons/>
+    <HomeIcons/>
+    <HomeIcons/>
+    <HomeIcons/>
     </section>
+    </section>
+    <Footer/>
+ </section>
 )
 
 
 export default Home;
+
+{/* <div className="title-section">
+<img src={logo} width="30%" alt=""/>
+<h1 className="title-text">
+    Best Software tool for Veterinary
+</h1>
+<span>
+    <Link to="/login">Login</Link> | <Link to="/signup">Signup</Link>
+</span>
+</div> */}
